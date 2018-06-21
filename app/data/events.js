3be7eb07aaba0d677924319f85f33c46c456dbe6
AngelaF <ADFrys@users.noreply.github.com>
2018-06-21 10:34:08 -0500

    Setup JSON file of events layout.

--- app/data/events.js
+++ app/data/events.js
@@ -1,100 +1,39 @@
 var events = [
 {
   "name":"xxxx",
-  "photo":"xxxx",
-  "scores": 
-  [
-      3,
-      5,
-      4,
-      3,
-      1,
-      4,
-      2,
-      2,
-      3,
-      1
-    ]
+  "date":"xxx",
+  "start-time":"xxxx",
+  "end-time": "xx"
   },
 {
-  "name":"Lisa Simpson",
-  "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Lisa_Simpson.png/220px-Lisa_Simpson.png",
-  "scores[]": [
-      2,
-      3,
-      5,
-      5,
-      2,
-      5,
-      1,
-      5,
-      3,
-      5
-    ]
+  "name":"xxxx",
+  "date":"xxx",
+  "start-time":"xxxx",
+  "end-time": "xx"
   },
 {
-  "name":"Pusheen",
-  "photo":"https://pbs.twimg.com/profile_images/848395594590814208/_TtPuzHs_400x400.jpg",
-  "scores[]": [
-      2,
-      1,
-      2,
-      5,
-      5,
-      2,
-      1,
-      1,
-      1,
-      1
-    ]
+  "name":"xxxx",
+  "date":"xxx",
+  "start-time":"xxxx",
+  "end-time": "xx"
   },
 {
-  "name":"Taz",
-  "photo":"http://www.cartoonspot.net/looney-tunes/wallpaper-images/taz-med9.jpg",
-  "scores[]": [
-      5,
-      3,
-      2,
-      1,
-      2,
-      2,
-      4,
-      1,
-      5,
-      5
-    ]
+  "name":"xxxx",
+  "date":"xxx",
+  "start-time":"xxxx",
+  "end-time": "xx"
   },
-  {
-  "name":"Professor Frink",
-  "photo":"https://www.selectspecs.com/blog/wp-content/uploads/2009/12/professorfrink1.gif",
-  "scores[]": [
-      1,
-      1,
-      5,
-      3,
-      5,
-      2,
-      1,
-      5,
-      1,
-      5
-    ]
+{
+  "name":"xxxx",
+  "date":"xxx",
+  "start-time":"xxxx",
+  "end-time": "xx"
   },
-  {
-  "name":"Tweety Bird",
-  "photo":"https://digitalmode.net/wp-content/uploads/2017/01/tweety-bird.png",
-  "scores[]": [
-      3,
-      5,
-      4,
-      5,
-      3,
-      3,
-      2,
-      2,
-      5,
-      2
-    ]
+{
+  "name":"xxxx",
+  "date":"xxx",
+  "start-time":"xxxx",
+  "end-time": "xx"
   }
 ];
 
